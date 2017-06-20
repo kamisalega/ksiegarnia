ccf22ef9733825c2980b3b6e26b8674b9ded2408
Kamil Sałęga <kamilsalega@gmail.com>
2017-06-20 14:22:08 +0200

    Pierwsza konfiguracja

--- src/main/java/pl/salega/KsiegarniaApplication.java
+++ src/main/java/pl/salega/KsiegarniaApplication.java
@@ -1,4 +1,4 @@
-package pl.salega.ksiegarnia;
+package pl.salega;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/pl/salega/KsiegarniaApplicationTests.java
+++ src/test/java/pl/salega/KsiegarniaApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.salega.ksiegarnia;
+package pl.salega;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
