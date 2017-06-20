3585cfd407b8733f1646f24b3d250c7f4925fa8b
Kamil Sałęga <kamilsalega@gmail.com>
2017-06-20 12:54:32 +0200

    Dodane pliki do .gitignore

---
+++ src/test/java/pl/salega/ksiegarnia/KsiegarniaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.salega.ksiegarnia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KsiegarniaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
