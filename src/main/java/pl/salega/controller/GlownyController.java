1c6a10549cdfcc1a1d0d40f6fdced2b9e613ee41
Kamil Sałęga <kamilsalega@gmail.com>
2017-06-21 09:13:08 +0200

    Stworzenie  "index.html" "style.css" i "GlownyController"

---
+++ src/main/java/pl/salega/controller/GlownyController.java
@@ -0,0 +1,19 @@
+package pl.salega.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by kamilsalega on 21.06.2017.
+ */
+
+@Controller
+public class GlownyController {
+
+    @RequestMapping("/")
+    public String index() {
+    return "index";
+    }
+
+
+}
